da200230c63d2c08c091c5af05c6c72a5ebbe078
Nick Robison <nick@nickrobison.com>
2018-05-21 11:21:51 -0700

    Foundation 6 migration. Getting closer, but topnav is totally borked.

--- lib/foundation.ml
+++ lib/foundation.ml
@@ -36,7 +36,7 @@ module Link = struct
 
   let side_nav (links:links) =
     let links = List.map (link ~cl:"") links in
-    mk_ul_links ~cl:"side-nav" ~links
+    mk_ul_links ~cl:"" ~links
 
   let bottom_nav (links:links) =
     let links = List.map (link ~cl:"") links in
@@ -72,8 +72,8 @@ module Index = struct
   let t ~top_nav =
     top_nav
     ++ br
-    ++ div ~cls:"row" (
-      div ~cls:"large-12 columns" (
+    ++ div ~cls:"grid-x" (
+      div ~cls:"cell large-12" (
         img (Uri.of_string "http://placehold.it/1000x400&amp;text=img")
         ++ hr
       ))
@@ -153,7 +153,7 @@ module Blog = struct
                 build_list (lnk :: acc) (x-1))
           in
           let link_list = previous_link :: build_list [next_link] (snd p) in
-          div ~cls:"row" (
+          div ~cls:"grid-x align-center" (
         nav ~attrs:["aria-label", "Pagination"]
           (ul ~add_li:false ~cls:"pagination text-center"
              link_list
@@ -166,18 +166,18 @@ module Blog = struct
       | Some s -> small (string s)
     in
     list [
-      div ~cls:"row"
-        (div ~cls:"large-9 columns" (h2 (string title ++ subtitle)));
-      div ~cls:"row" (
-        div ~cls:"small-12 large-9 columns" ~attrs:["role", "content"] posts
-        ++ aside ~cls:"small-12 large-3 columns panel" sidebar
+      div ~cls:"grid-x"
+        (div ~cls:"cell large-9" (h2 (string title ++ subtitle)));
+      div ~cls:"grid-x grid-margin-x" (
+        div ~cls:"cell large-7 large-offset-2" ~attrs:["role", "content"] posts
+        ++ aside ~cls:"cell large-2 callout secondary" sidebar
       );
       page_links;
-      footer ~cls:"row" (
-        div ~cls:"large-12 columns" (
+      footer ~cls:"grid-x" (
+        div ~cls:"cell" (
           hr
-          ++ div ~cls:"row" (
-            div ~cls:"large-6 columns" (
+          ++ div ~cls:"grid-x" (
+            div ~cls:"cell large-6" (
               p (small (string "&copy; Copyright " ++ copyright))
             ))))
     ]
@@ -224,31 +224,28 @@ let body ?google_analytics ?highlight ~title:t ~headers ~content ~trailers () =
       title (string t);
       link ~rel:"stylesheet" (Uri.of_string "/css/foundation.min.css");
       link ~rel:"stylesheet" (Uri.of_string "/css/site.css");
-      script ~src:(Uri.of_string "/js/vendor/custom.modernizr.js") empty;
       highlight_css;
       ga;
       headers;
     ])
   ++ body (list [
       content;
-      script ~src:(Uri.of_string "/js/vendor/jquery.min.js") empty;
-      script ~src:(Uri.of_string "/js/foundation/foundation.min.js") empty;
-      script ~src:(Uri.of_string "/js/foundation/foundation.topbar.js") empty;
+      script ~src:(Uri.of_string "/js/vendor/jquery.js") empty;
+      script ~src:(Uri.of_string "/js/vendor/foundation.min.js") empty;
       script js_init;
       highlight_trailer;
       trailers
     ])
 
 let top_nav ~title ~title_uri ~nav_links =
-  div ~cls:"contain-to-grid fixed" (
-    nav ~cls:"top-bar" ~attrs:["data-topbar",""] (
-      ul ~add_li:false ~cls:"title-area" [
-        li ~cls:"name" (h1 (a ~href:title_uri title));
-        li ~cls:"toggle-topbar menu-icon"
-          (a ~href:(Uri.of_string "#") (span (string "Menu")));
+  div ~cls:"top-bar" ~attrs:["data-topbar",""] (
+      div ~cls:"top-bar-left" (
+        ul ~add_li:false ~cls:"dropdown menu" ~attrs: ["data-dropdown-menu", ""][
+          li ~cls:"menu-text" (a ~href:title_uri title);
+          nav_links;
       ]
-      ++ section ~cls:"top-bar-section" nav_links
-    ))
+      )
+    )
 
 let page ~body =
   Printf.sprintf
